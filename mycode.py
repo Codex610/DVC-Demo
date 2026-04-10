553573a01a095e66f9847807f65eb5291883d411
Rajneesh chauhan <rajnishchauhan912556@gmail.com>
2026-04-10 17:04:41 +0530

    Third version of data saved

--- mycode.py
+++ mycode.py
@@ -12,6 +12,10 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name':'GF1','Age':20,'City':'Lucknow'}
 df.loc[len(df.index)] = new_row_loc
 
+## Adding new row to df for V3
+new_row_loc = {'Name':'GF2','Age':30,'City':'Delhi'}
+df.loc[len(df.index)] = new_row_loc
+
 ## Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
